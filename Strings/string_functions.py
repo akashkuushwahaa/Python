05cd399573773d34314bcf5c769f77c72085ec31
akashkuushwahaa <akashkushwaha76986@gmail.com>
2025-07-01 18:40:58 +0530

    Strings

---
+++ Strings/string_functions.py
@@ -0,0 +1,8 @@
+s = "hello world"
+print(len(s))
+print(s.count("l"))
+print(s.title())
+print(s.lower())
+print(s.upper())
+print(s.find("world"))
+print(s.replace("hello", "hi"))
---
+++ Strings/string_indexing_slicing.py
@@ -0,0 +1,4 @@
+s = "Python"
+print("First character:", s[0])
+print("Last character:", s[-1])
+print("Slicing [1:4]:", s[1:4])
