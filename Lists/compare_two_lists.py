32056319601bda9a0231f3ebc4d19f182b7f90a9
akashkuushwahaa <akashkushwaha76986@gmail.com>
2025-07-01 18:42:21 +0530

    Lists

---
+++ Lists/compare_two_lists.py
@@ -0,0 +1,3 @@
+list1 = [1, 2, 3]
+list2 = [1, 2, 3]
+print("Lists are equal" if list1 == list2 else "Lists are not equal")
---
+++ Lists/list_insert_delete_reverse.py
@@ -0,0 +1,6 @@
+st = [9, 8, 7, 6, 5, 4, 3]
+st.insert(0, 11)
+del st[5]
+st.append(10)
+print("Reversed:", st[::-1])
+
